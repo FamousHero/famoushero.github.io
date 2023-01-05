cbc913ce110db4573154723f9bdf2d8283a72d87
FamousHero <danielfranco2011@gmail.com>
2023-01-05 08:32:13 -0800

    Fix page scrolling w flexbox items
    Margin causes sticking while translate does not

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ document.addEventListener('beforeunload', (e)=>{
 
 var index = 2;
 var showcaseTimeoutID = null;
+var currShowcaseButton = document.getElementById('showcase1');
+var firstShowcase = document.querySelector('#Projects .inner-section .flexbox-container');
 function autoScroll(index){
-    document.getElementById('showcase'+index).checked = true;
+    let previndex = index-1;
+    if(previndex < 1)
+        previndex = 3;
+    document.getElementById('showcase'+previndex).classList.remove('checked');
+    document.getElementById('showcase'+index).classList.add('checked');
+    transformFlexbox(index);
+    currShowcaseButton = document.getElementById('showcase'+index);
     //need to refer to the global variable and set it to local+1
     this.index = parseInt(index)+1;
     if(this.index > 3)
@@ -18,10 +26,34 @@ function autoScroll(index){
     showcaseTimeoutID = setTimeout(autoScroll, 4000, this.index);
 }
 showcaseTimeoutID = setTimeout(autoScroll, 4000, index);
+
 var showcaseButtons = document.querySelectorAll('.manual-button');
 showcaseButtons.forEach(button => {
     button.addEventListener('click', ()=>{
+        currShowcaseButton.classList.remove('checked');
+        currShowcaseButton = button;
+        button.classList.add('checked');
+        let index = button.dataset.showcase;
+        transformFlexbox(index);
         clearTimeout(showcaseTimeoutID);
         showcaseTimeoutID = setTimeout(autoScroll, 4000, button.dataset.nextShowcase);
     })
 });
+
+function transformFlexbox(index){
+    firstShowcase.style.transform = 'translate(0,' + (-500 * (Number(index)-1)) + 'px)';
+}
+/*
+#showcase1:checked  ~ div.first{
+    margin-top: 0;
+}
+#showcase2:checked ~ div.first{
+    margin-top: -450px;
+}
+#showcase3:checked ~ div.first{
+    margin-top: -900px;
+}
+#showcase4:checked ~ div.first{
+    margin-left: 0;
+}
+*/
