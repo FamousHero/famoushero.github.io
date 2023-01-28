becf0d288f65419b55b7e2c1972eb6a037a8a831
Daniel Franco <danielfranco2011@gmail.com>
2023-01-27 21:20:25 -0800

    Move cv-project to sep repo

--- pages/TOP-Assignments/cv-project/src/Components/Button.js
+++
@@ -1,9 +0,0 @@
-import './button.css'
-
-const Button = ({text, onClick}) => {
-  return (
-    <button className="button" type='button' onClick={onClick}>{text}</button>
-  )
-}
-
-export default Button
