3d7a56508765d25c9f119a4f02818a1062e14f3c
Daniel Franco <danielfranco2011@gmail.com>
2023-02-23 02:32:06 -0800

    Link home page to dist/index.html for Lib

--- pages/TOP-Assignments/Library/webpack.config.js
+++ pages/TOP-Assignments/Library/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devtool: 'source-map',
     devServer: {
         static: {
             directory: path.resolve(__dirname, 'dist')
