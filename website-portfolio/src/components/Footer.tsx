be502a851b0ad0d09f1d64a28d721c6311b0903a
Daniel Franco <danielfranco2011@gmail.com>
2023-10-25 02:26:11 -0700

    Create header & footer components

---
+++ website-portfolio/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='text-2xl bg-gray-300'>
+        Finna stick my foot
+    </footer>
+  )
+}
+
+export default Footer
---
+++ website-portfolio/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='w-full absolute z-10 bg-black text-white py-4'>
+      <nav className='max-w-[1440px] mx-auto flex justify-between '>
+        <div>
+          Start of page navbar
+        </div>
+        <div>End of page navbar</div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
