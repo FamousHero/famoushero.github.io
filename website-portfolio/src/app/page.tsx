22e4aad771b221faf7ced99a695cecd7769575d2
FamousHero <danielfranco2011@gmail.com>
2024-05-03 08:47:21 -0700

    app: Create Porjects.json to easily add/remove new projects.

--- website-portfolio/src/app/page.tsx
+++ website-portfolio/src/app/page.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
 import Project from '@/components/Project'
+import Projects from './Projects.json' assert {type : 'json'}
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section id="Projects">
-        <Project name= 'Spotify Playlist Search' gallery= {[]} description=''></Project>
+      <section className='flex justify-around w-[80%]' id="Projects">
+        {
+          Projects["Projects"].map((project, i) =>
+            (
+              <Project key={i} name={project.name} description={project.description}
+              gallery={project.gallery}></Project>
+            )
+          )
+        }
       </section>
     </main>
   )
