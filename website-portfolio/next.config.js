e94f5970d99ec873ef5ccff4a8047eccfe522cd8
FamousHero <danielfranco2011@gmail.com>
2024-05-11 05:02:28 -0700

    Fix: Update package.json to fix vulnerabilities

--- website-portfolio/next.config.js
+++ website-portfolio/next.config.js
@@ -1,8 +1,12 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} 
+ * 
 const nextConfig = {
     output: "export",
     reactStrictMode: true,
     basePath: "/website-portfolio"
 }
 
+module.exports = nextConfig
+*/
+const nextConfig = {}
 module.exports = nextConfig
--- website-portfolio/src/components/Navbar.tsx
+++ website-portfolio/src/components/Navbar.tsx
@@ -10,16 +10,15 @@ const Navbar = () => {
     <header className='w-full fixed z-10 bg-black text-white  py-4 border-b-2 border-white/70'>
       <nav className='max-w-[1440px] mx-auto flex justify-start gap-6 px-2 '>
         {navList.map((item, index)=>{
-        let classes = 'rounded-md px-3 py-2 text-small font-medium text-gray-300 \
-        focus:bg-gray-900 focus:text-white hover:bg-neutral-800/50 hover:text-white \
+        let classes = 'rounded-md w-[80px] h-[40px] px-3 py-2 text-small font-medium text-gray-300 \
+         hover:bg-neutral-800/50 hover:text-white \
         '
         //Want the focus of a button and functionality of a tag 
         //Didn't want to make a click function just for that
-        return ( <button key={index} className= {classes}>
-                  <a href={item.href}>
+        return (  <a className={classes} href={item.href}>
                     {item.title}
                   </a>
-                </button>)
+               )
         }
         )}
       </nav>
