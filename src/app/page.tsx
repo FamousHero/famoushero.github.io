3c36c1cd0250986829f427cd30daa232f7ca8d14
Daniel <danielfranco2011@gmail.com>
2024-05-14 17:41:39 -0700

    Use async outside Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
+import { use } from 'react'
 import Image from 'next/image'
 import Head from 'next/head'
 import About from '@/components/About'
 import Projects_container from '@/components/Projects_container'
 import Socials from '@/components/Socials'
 import {headers} from 'next/headers'
-export default function Home() {
-  const header = headers();
-  const ip =  (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0].replace(/^.*:/, '');
+async function ipLogger(){
+  const res = (await fetch("https://api.ipify.org?format=json")).json();
+  if(!res){
+    throw new Error('Failed to fetch');
+  }
+  return res;
+}
+export default async function Home() {
+  const data = await ipLogger();
+  console.log(data);
   return (
     <main className="min-h-screen">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <h1 id='About' className='pt-[100px] text-center text-3xl text-fade font-bold'>About {ip}</h1>
+      <h1 id='About' className='pt-[100px] text-center text-3xl text-fade font-bold'>About {JSON.stringify(data)}</h1>
       <About/>
       <h1 id='Projects' className='pt-[100px] text-center text-3xl text-fade font-bold'>Projects</h1>
       <Projects_container/>
