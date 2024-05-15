8818eb7f1ddd9e286076e19d21125e6a43ea00f2
Daniel <danielfranco2011@gmail.com>
2024-05-14 18:00:36 -0700

    use client for page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,28 @@
-import { use } from 'react'
+'use client'
+ import { useEffect } from 'react'
 import Image from 'next/image'
 import Head from 'next/head'
 import About from '@/components/About'
 import Projects_container from '@/components/Projects_container'
 import Socials from '@/components/Socials'
 import {headers} from 'next/headers'
-async function ipLogger(){
-  const res = (await fetch("https://api.ipify.org?format=json")).json();
-  if(!res){
-    throw new Error('Failed to fetch');
+
+export default function Home() {
+  const IPFetch = useEffect(() =>{
+  const fetchData = async () =>{
+    const res = (await fetch("https://api.ipify.org?format=json")).json();
+    if(!res){
+      throw new Error('Failed to fetch');
+    }
+    return res;
   }
-  return res;
-}
-export default async function Home() {
-  const data = await ipLogger();
-  console.log(data);
+},[])
   return (
     <main className="min-h-screen">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <h1 id='About' className='pt-[100px] text-center text-3xl text-fade font-bold'>About {JSON.stringify(data)}</h1>
+      <h1 id='About' className='pt-[100px] text-center text-3xl text-fade font-bold'>About {JSON.stringify(IPFetch)}</h1>
       <About/>
       <h1 id='Projects' className='pt-[100px] text-center text-3xl text-fade font-bold'>Projects</h1>
       <Projects_container/>
